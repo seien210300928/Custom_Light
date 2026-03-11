f64b014e73868c241978dea8f245e9476c0d91b5
seien <seien@210300928@outlook.com>
2026-03-11 12:46:56 +0800

    1.21.7更新

--- src/main/java/org/dpdns/seien/custom_light/Custom_light.java
+++ src/main/java/org/dpdns/seien/custom_light/Custom_light.java
@@ -130,7 +130,7 @@ public class Custom_light {
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    @EventBusSubscriber(modid = MODID, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
